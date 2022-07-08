8bbbf3ccf3af77f81c4a8ee7ecaa51d6c625b9f4
Sean Sarokas <sps4462@gmail.com>
2022-07-08 06:51:12 -0400

    lab complete

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,29 @@
 import React from "react";
+import FilterButton from "./FilterButton.js";
 
-function CategoryFilter() {
+function CategoryFilter({ categories, onFilter }) {
+
+  // const renderButtons = categories.map(category => {
+  //   return (
+  //     <button 
+  //       className="" 
+  //       key={category} 
+  //       onClick={() => onFilter(category)}
+  //     >
+  //     {category}
+  //     </button>
+  //   )
+  // })
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
       {/* render <button> elements for each category here */}
+      {categories.map(category =>{
+        return (
+          <FilterButton catText={category} key={category} filter={onFilter}/>
+        )
+      })}
     </div>
   );
 }
